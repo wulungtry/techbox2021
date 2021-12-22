a64b2117cd2ef306b1aa0a7a28708cfefda0e09f
wulung-triyanto <wulung.triyanto@sera.astra.co.id>
2021-12-22 10:10:00 +0700

    20211222-intial commit

---
+++ Azure.SQL.Sample/Azure.SQL.Sample/dbo/Tables/MasterAddressType.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[MasterAddressType]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL , 
+    [AddressTypeCode] VARCHAR(25) NOT NULL, 
+    [AddressTypeName] VARCHAR(50) NOT NULL, 
+    [CreatedAt] DATETIME NOT NULL, 
+    [CreatedBy] VARCHAR(50) NOT NULL, 
+    [UpdatedAt] DATETIME NULL, 
+    [UpdatedBy] VARCHAR(50) NULL, 
+    [DeletedAt] NCHAR(10) NULL, 
+    [DeletedBy] VARCHAR(50) NULL, 
+    CONSTRAINT [PK_MasterAddressType] PRIMARY KEY (Id)
+)
---
+++ Azure.SQL.Sample/Azure.SQL.Sample/dbo/Tables/MasterIdentityType.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[MasterIdentityType]
+(
+	[Id] INT NOT NULL IDENTITY , 
+    [IdentityTypeCode] VARCHAR(25) NOT NULL, 
+    [IdentityName] VARCHAR(50) NOT NULL,
+    [CreatedAt] DATETIME NOT NULL, 
+    [CreatedBy] VARCHAR(50) NOT NULL, 
+    [UpdatedAt] DATETIME NULL, 
+    [UpdatedBy] VARCHAR(50) NULL, 
+    [DeletedAt] NCHAR(10) NULL, 
+    [DeletedBy] VARCHAR(50) NULL, 
+    CONSTRAINT [PK_MasterIdentityType] PRIMARY KEY ([Id])
+)
